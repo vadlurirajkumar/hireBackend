d68bf76510f16edcdeb830738ec6f75bdd02ed6e
Vadluri Rajkumar <vadlurirajkumar6@gmail.com>
2023-01-31 13:12:36 +0530

    second

--- config/DBconnection.js
+++ config/DBconnection.js
@@ -3,10 +3,7 @@ mongoose.set("strictQuery", true); //^ just for mongoDB warning
 
 const DBconnection = async () => {
   try {
-    const { connection } = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const { connection } = await mongoose.connect(process.env.MONGO_URI);
     console.log(`DB Connected : ${connection.host}`.cyan.underline);
   } catch (error) {
     console.log(`Error is : ${error.message}`.red.bold);
