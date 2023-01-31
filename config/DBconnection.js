a3e1ab6ca34a447a9c4717e74dd9d47d10f15abd
Vadluri Rajkumar <vadlurirajkumar6@gmail.com>
2023-01-31 14:06:58 +0530

    second-1

--- config/DBconnection.js
+++ config/DBconnection.js
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
-mongoose.set("strictQuery", true); //^ just for mongoDB warning
+mongoose.set("strictQuery", true);
 
 const DBconnection = async () => {
   try {
-    const { connection } = await mongoose.connect(process.env.MONGO_URI);
+    const uri = process.env.MONGO_URI;
+    if (typeof uri !== "string") {
+      throw new Error(
+        `The 'uri' parameter to 'openUri()' must be a string, got "${typeof uri}". Make sure the first parameter to 'mongoose.connect()' or 'mongoose.createConnection()' is a string.`
+      );
+    }
+    const { connection } = await mongoose.connect(uri);
     console.log(`DB Connected : ${connection.host}`.cyan.underline);
   } catch (error) {
     console.log(`Error is : ${error.message}`.red.bold);
