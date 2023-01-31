8671ee3ccda0b18bbe7ad2910baccd9f14ed33e6
Vadluri Rajkumar <vadlurirajkumar6@gmail.com>
2023-01-31 14:15:08 +0530

    thrd

--- config/DBconnection.js
+++ config/DBconnection.js
@@ -1,19 +1,25 @@
 import mongoose from "mongoose";
-mongoose.set("strictQuery", true);
+// mongoose.set("strictQuery", true); //^ just for mongoDB warning
 
+// const DBconnection = async () => {
+//   try {
+//     const { connection } = await mongoose.connect(process.env.MONGO_URI);
+//     console.log(`DB Connected : ${connection.host}`.cyan.underline);
+//   } catch (error) {
+//     console.log(`Error is : ${error.message}`.red.bold);
+//     process.exit(1);
+//   }
+// };
+
+// const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
 const DBconnection = async () => {
   try {
-    const uri = process.env.MONGO_URI;
-    if (typeof uri !== "string") {
-      throw new Error(
-        `The 'uri' parameter to 'openUri()' must be a string, got "${typeof uri}". Make sure the first parameter to 'mongoose.connect()' or 'mongoose.createConnection()' is a string.`
-      );
-    }
-    const { connection } = await mongoose.connect(uri);
-    console.log(`DB Connected : ${connection.host}`.cyan.underline);
-  } catch (error) {
-    console.log(`Error is : ${error.message}`.red.bold);
-    process.exit(1);
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("db is connected");
+  } catch (err) {
+    console.log(err);
   }
 };
 export { DBconnection };
